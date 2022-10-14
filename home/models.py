856cfaaacab944f375fc48f96c65ac3d709b0fc4
Varun Singh <varun14gb@gmail.com>
2022-10-15 00:14:12 +0530

    added Job model

--- home/models.py
+++ home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Job(models.Model):
+    role = models.CharField(max_length = 255)
+    description = models.TextField()
+    ctc = models.CharField(max_length = 10)
+    profile = models.CharField(max_length = 255)
+    company = models.CharField(max_length = 255)
+    location = models.CharField()
