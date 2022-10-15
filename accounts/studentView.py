71a673f1282fe9c61d0bddd165f9eef89b46b0fe
Anup Kumar Mridha <anupkumarmridha.net@gmail.com>
2022-10-15 06:50:13 +0530

    created student view

---
+++ accounts/studentView.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from accounts import studentView
+
+
+def addStudentProfileDetails(request):
+    pass
+def updateStudentProfileDetails(request):
+    pass
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from accounts import views
 from accounts import resetPasswordView
+from accounts import studentView
 
 urlpatterns = [
   path('/signup/', views.handelSingup, name='handelSingup'),
@@ -9,9 +10,11 @@ urlpatterns = [
   path('/logout', views.handleLogout, name='handleLogout'),
   
   # to reset password
-    path('activate/<uidb64>/<token>', views.activate, name='activate'),
-    path("password_change", resetPasswordView.password_change, name="password_change"),
-    path("password_reset", resetPasswordView.password_reset_request, name="password_reset"),
-    path('reset/<uidb64>/<token>', resetPasswordView.passwordResetConfirm, name='password_reset_confirm'),
+  path('activate/<uidb64>/<token>', views.activate, name='activate'),
+  path("password_change", resetPasswordView.password_change, name="password_change"),
+  path("password_reset", resetPasswordView.password_reset_request, name="password_reset"),
+  path('reset/<uidb64>/<token>', resetPasswordView.passwordResetConfirm, name='password_reset_confirm'),
+  path('/addStudentProfileDetails/', studentView.addStudentProfileDetails, name='addStudentProfileDetails'),
+  path('/updateStudentProfileDetails/',studentView.updateStudentProfileDetails, name='updateStudentProfileDetails'),
 #   path('/admin', views.company_home, name='admin_home'),
 ]
